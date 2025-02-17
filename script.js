c9986f8fe1f5f470429b2bc71d19135f68660331
AndersonMendesdeSouza <andersonmendesdesouza2007@gmail.com>
2025-02-16 21:55:33 -0300

    Subindo atualizações

--- script.js
+++ script.js
@@ -2,20 +2,32 @@
 
 
  function CalculaDolar(event){
+
     event.preventDefault();
    var ul = document.getElementById("ul")
    var li = document.createElement("li");
     var Real = document.getElementById('real').value;
     var Dolar = document.getElementById('dolar').value;
 
-    var total = Real / Dolar;
+    if(Real === '' || Dolar === ''){
+alert("Por favor coloque um valor!");
+return false;
+    }else{
+      var total = Real / Dolar;
     
 
-    li.innerHTML = total;
- ul.appendChild(li);
- setTimeout(() => {
-    var Real = document.getElementById('real').value = '';
-    var Dolar = document.getElementById('dolar').value = '';
- }, 2000);
+      li.innerHTML = total;
+   ul.appendChild(li);
+   setTimeout(() => {
+      var Real = document.getElementById('real').value = '';
+      var Dolar = document.getElementById('dolar').value = '';
+   }, 0);
+    }
+
+
 
  }
+ function reset(){
+   var ul = document.getElementById("ul");
+ul.innerHTML = '';
+}
